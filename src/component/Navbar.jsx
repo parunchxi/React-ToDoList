ebe59056e224511b2c61daeea6b4cf6abfd3289c
parunchxi <parunchxi@gmail.com>
2023-06-14 22:07:00 +0700

    Update navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,13 +1,10 @@
-import { RiMoonFill, RiGithubFill } from "react-icons/ri";
+import { RiGithubFill } from "react-icons/ri";
 import styles from "./Navbar.module.scss";
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div>
-        <a id="change_theme">
-          <RiMoonFill />
-        </a>
         <a id="github" href="https://github.com/parunchxi/React-ToDoList">
           <RiGithubFill />
         </a>
