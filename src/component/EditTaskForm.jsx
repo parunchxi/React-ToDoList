9aa0058c0640b7e87fb87314e883974d877a9af9
parunchxi <parunchxi@gmail.com>
2023-06-14 21:57:13 +0700

    Update style

--- src/component/EditTaskForm.jsx
+++ src/component/EditTaskForm.jsx
@@ -10,7 +10,7 @@ function EditTaskForm({
 }) {
   return (
     <div className={styles.edit_form}>
-      <div className="card">
+      <div className={`card ${styles.card}`}>
         <h1>Edit Task</h1>
         <form onSubmit={handleEditSubmit} className="input_form">
           <input
